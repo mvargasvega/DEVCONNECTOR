5f8170d54b960d7b59e4d34cb3c68015c2ed2040
mvargasvega <martinvargasvega@gmail.com>
2019-08-07 13:34:13 -0700

    Created async functions that will do form validation to make sure that all the data fields are filled out. Also created a function to check for any errors.  Created methods to check if we have duplicate entries and encrypting the users password

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     },
     avatar: {
         type: String
-    }
+    },
     date: {
         type: Date,
         default: Date.now
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require("express-validator/check");
 
+const User = require('../../models/User');
 // @route GET api/users
 // @desc Test Route
 // @acess Public
 
 // router.get('/',(req,res) => res.send ('User Routed'));
-
+// validation for the user to put
 router.post("/",
     [
         check("name", "Name is required")
@@ -19,13 +22,49 @@ router.post("/",
             "Please enter a password with 6 or more characters"
         ).isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        console.log(req.body);
-        res.send("User was create succesffuly");
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if(user) {
+                return res.status(400).json({ errors: [ { msg: 'User already exists' }] });
+            }
+        // see if user exits
+
+        // get user gravatar
+        const  avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+
+
+        // encrypt password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        
+        // return jsonwebToken
+        res.send("User registered");
+        }catch(err){
+            console.log(err.message);
+            res.status(500).send('Server error');
+        }
     }
 );
 
